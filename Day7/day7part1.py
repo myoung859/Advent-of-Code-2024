10a1379a850ba2bd11b0b96ba1d27051b51d562c
myoung72 <myoung72@jhu.edu>
2026-05-01 19:47:10 -0500

    day 7 done

---
+++ Day7/day7part1.py
@@ -0,0 +1,20 @@
+import re
+import itertools
+
+f = open("day7.txt")
+total_cal = 0
+for line in f.readlines():
+    nums = [int(i) for i in re.findall(r"\d+", line)]
+    target = nums[0]
+    test_nums = nums[1:]
+    for ops in itertools.product(("+", "*"), repeat=len(test_nums) - 1):
+        result = test_nums[0]
+        for i in range(0, len(ops)):
+            if ops[i] == "+":
+                result += test_nums[i + 1]
+            elif ops[i] == "*":
+                result *= test_nums[i + 1]
+        if result == target:
+            total_cal += target
+            break
+print(total_cal)
